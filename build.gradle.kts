a385b0052a3026b606a35262e589ab6374082343
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-09-02 22:43:01 +0300

    изменил настройки gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,52 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+    java
+    kotlin("jvm") version "1.7.10"
+    id("org.springframework.boot") version "2.6.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("plugin.spring") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "tv.codealong.tutorials.springboot"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+
+val javaVersion = JavaVersion.VERSION_11
+val springmockkVersion = "4.0.2"
+val mockkVersion = "1.13.4"
+
+java.sourceCompatibility = javaVersion
+java.targetCompatibility = javaVersion
+
+extra["kotlin.version"] = "1.7.10"
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenLocal()
+    mavenCentral()
+//	maven { url = uri("https://repo.spring.io/milestone") }
+//	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.mockk:mockk:1.10.4")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    runtimeOnly("org.jetbrains.kotlin:kotlin-bom")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("com.ninja-squad:springmockk:${springmockkVersion}")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
