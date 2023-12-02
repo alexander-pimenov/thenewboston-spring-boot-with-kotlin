4619733ac49e6f7d675a6358be003f6ea5b9d332
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-12-02 16:54:45 +0300

    добавил ObjectMapper и тесты к нему

---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/service/ObjectMapperService.kt
@@ -0,0 +1,10 @@
+package tv.codealong.tutorials.springboot.thenewboston.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Service
+
+@Service
+class ObjectMapperService(objectMapper: ObjectMapper) {
+
+
+}
