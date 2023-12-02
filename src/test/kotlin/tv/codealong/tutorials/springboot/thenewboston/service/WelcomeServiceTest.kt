67106a00c21482af6d7d170d8c0becc5ac7e53df
Alexander Pimenov <alexander.pimenov.s@yandex.ru>
2023-12-02 17:26:43 +0300

    добавил TestFactory для тестов  WelcomeServiceTest

--- src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/service/WelcomeServiceTest.kt
+++ src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/service/WelcomeServiceTest.kt
@@ -1,6 +1,8 @@
 package tv.codealong.tutorials.springboot.thenewboston.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -22,5 +24,16 @@ internal class WelcomeServiceTest {
             Arguments.of("Boss", "Welcome Boss!"),
             Arguments.of("Gosha", "Welcome Gosha!")
         )
+    }
+
+    @TestFactory
+    fun `Parameterized Different names`() = listOf(
+        "Sasha" to "Welcome Sasha!",
+        "Boss" to "Welcome Boss!",
+        "Gosha" to "Welcome Gosha!"
+    ).map { (input, expected) ->
+        DynamicTest.dynamicTest("Greeting of $input should be $expected") {
+            assertEquals(expected, welcomeService.getWelcomeMessage(input))
+        }
     }
 }
